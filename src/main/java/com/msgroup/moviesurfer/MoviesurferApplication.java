385550977c6970d768cd272f93d3b39f25bb5ab0
AbdullahHinnawi <hinnawiabdullah@gmail.com>
2020-01-29 13:37:43 +0200

    new spring app created

---
+++ src/main/java/com/msgroup/moviesurfer/MoviesurferApplication.java
@@ -0,0 +1,13 @@
+package com.msgroup.moviesurfer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviesurferApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoviesurferApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/msgroup/moviesurfer/MoviesurferApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msgroup.moviesurfer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesurferApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
