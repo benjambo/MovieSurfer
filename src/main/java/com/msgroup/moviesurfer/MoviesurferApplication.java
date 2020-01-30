f255debd99e7a578a6e76b15d180bf9da949821f
Jepu32 <43566068+Jepu32@users.noreply.github.com>
2020-01-30 14:34:52 +0200

    Code for hibernate

--- src/main/java/com/msgroup/moviesurfer/MoviesurferApplication.java
+++ src/main/java/com/msgroup/moviesurfer/MoviesurferApplication.java
@@ -1,16 +1,25 @@
 package com.msgroup.moviesurfer;
 
+import com.msgroup.moviesurfer.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MoviesurferApplication {
+public class MoviesurferApplication implements CommandLineRunner {
+
+    @Autowired
+    UserRepository repository;
 
     public static void main(String[] args) {
 
         SpringApplication.run(MoviesurferApplication.class, args);
 
-
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
 }
