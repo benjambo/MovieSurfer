21563951f1327db59e822b2a4273890eb17d044e
AbdullahHinnawi <hinnawiabdullah@gmail.com>
2020-01-30 15:24:58 +0200

    CÃlasses: Usercontroller, UserService, UserRepository added

---
+++ src/main/java/com/msgroup/moviesurfer/services/UserService.java
@@ -0,0 +1,19 @@
+package com.msgroup.moviesurfer.services;
+
+import com.msgroup.moviesurfer.model.User;
+import com.msgroup.moviesurfer.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public User saveUser(User user){
+
+       return userRepository.save(user);
+
+    }
+}
