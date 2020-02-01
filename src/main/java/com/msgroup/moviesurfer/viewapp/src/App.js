34504d047d48533a34564252fd6f137c5c51dbf8
Benjamin Bowo <ben.bowo@gmail.com>
2020-02-01 16:40:30 +0200

    Images

--- src/main/java/com/msgroup/moviesurfer/viewapp/src/App.js
+++ src/main/java/com/msgroup/moviesurfer/viewapp/src/App.js
@@ -1,22 +1,20 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>The Best Movie Application for You!</p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/benjambo/MovieSurfer"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          MovieSurfer Development Teams Github
         </a>
       </header>
     </div>
