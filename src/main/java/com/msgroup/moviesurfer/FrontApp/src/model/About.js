1d44238a77e814cd905b9dc645f03e9256dfa7c2
wwaltsu <walter.ruoppa@metropolia.fi>
2020-02-06 12:47:52 +0200

    about page muokkausta

--- src/main/java/com/msgroup/moviesurfer/FrontApp/src/model/About.js
+++ src/main/java/com/msgroup/moviesurfer/FrontApp/src/model/About.js
@@ -7,6 +7,7 @@ export const About = () => {
         <li data-target="#myCarousel" data-slide-to="0" className="active"></li>
         <li data-target="#myCarousel" data-slide-to="1"></li>
         <li data-target="#myCarousel" data-slide-to="2"></li>
+        <li data-target="#myCarousel" data-slide-to="3"></li>
       </ol>
       <div className="carousel-inner">
         <div className="carousel-item active">
@@ -14,11 +15,10 @@ export const About = () => {
                src="https://images.squarespace-cdn.com/content/v1/59d2bea58a02c78793a95114/1524518669406-2D0YA93DDYGDKYHXOZUU/ke17ZwdGBToddI8pDm48kKFYOtK8W4VIIm-D7unIF7lZw-zPPgdn4jUwVcJE1ZvWQUxwkmyExglNqGp0IvTJZUJFbgE-7XRK3dMEBRBhUpwC26LRMJwK8bCYeHqiJdGGAY7jUqG9Ai63KPiOW2tVcFehCc4d35tVad10Z5dYSro/Screen+Shot+2018-04-23+at+5.23.41+PM.png?format=2500w"
                alt="First slide"/>
             <div className="container">
-              <div className="carousel-caption text-left">
-                <h1>Example headline.</h1>
+              <div className="carousel-caption">
+                <h1>Walter Ruoppa</h1>
                 <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at
                   eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-                <p><a className="btn btn-lg btn-primary" href="#myCarousel" role="button">Sign up today</a></p>
               </div>
             </div>
         </div>
@@ -28,26 +28,37 @@ export const About = () => {
                alt="Second slide"/>
             <div className="container">
               <div className="carousel-caption">
-                <h1>Another example headline.</h1>
+                <h1>Abdullah Hinnawi</h1>
                 <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at
                   eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-                <p><a className="btn btn-lg btn-primary" href="#myCarousel" role="button">Learn more</a></p>
               </div>
             </div>
+        </div>
+        <div className="carousel-item">
+          <img className="fourth-slide"
+               src="https://images.squarespace-cdn.com/content/v1/59d2bea58a02c78793a95114/1524518669406-2D0YA93DDYGDKYHXOZUU/ke17ZwdGBToddI8pDm48kKFYOtK8W4VIIm-D7unIF7lZw-zPPgdn4jUwVcJE1ZvWQUxwkmyExglNqGp0IvTJZUJFbgE-7XRK3dMEBRBhUpwC26LRMJwK8bCYeHqiJdGGAY7jUqG9Ai63KPiOW2tVcFehCc4d35tVad10Z5dYSro/Screen+Shot+2018-04-23+at+5.23.41+PM.png?format=2500w"
+               alt="Second slide"/>
+          <div className="container">
+            <div className="carousel-caption">
+              <h1>Benjamin Bowo</h1>
+              <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at
+                eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+            </div>
+          </div>
         </div>
         <div className="carousel-item">
           <img className="third-slide"
                src="https://images.squarespace-cdn.com/content/v1/59d2bea58a02c78793a95114/1524518669406-2D0YA93DDYGDKYHXOZUU/ke17ZwdGBToddI8pDm48kKFYOtK8W4VIIm-D7unIF7lZw-zPPgdn4jUwVcJE1ZvWQUxwkmyExglNqGp0IvTJZUJFbgE-7XRK3dMEBRBhUpwC26LRMJwK8bCYeHqiJdGGAY7jUqG9Ai63KPiOW2tVcFehCc4d35tVad10Z5dYSro/Screen+Shot+2018-04-23+at+5.23.41+PM.png?format=2500w"
                alt="Third slide"/>
             <div className="container">
-              <div className="carousel-caption text-right">
-                <h1>One more for good measure.</h1>
+              <div className="carousel-caption">
+                <h1>Jere Saarelma</h1>
                 <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at
                   eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-                <p><a className="btn btn-lg btn-primary" href="#myCarousel" role="button">Browse gallery</a></p>
               </div>
             </div>
         </div>
+
       </div>
       <a className="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
         <span className="carousel-control-prev-icon" aria-hidden="true"></span>
