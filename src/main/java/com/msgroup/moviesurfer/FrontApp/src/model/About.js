086d74cf3ae75df2092ab7b42fe81591a425b24f
wwaltsu <walter.ruoppa@metropolia.fi>
2020-02-06 17:02:36 +0200

    test test benin koneella

--- src/main/java/com/msgroup/moviesurfer/FrontApp/src/model/About.js
+++ src/main/java/com/msgroup/moviesurfer/FrontApp/src/model/About.js
@@ -1,85 +1,67 @@
 import React from "react";
 //src/main/java/com/msgroup/moviesurfer/FrontApp/src/assets/jere.jpg
-import jere from "../assets/jere.jpg";
-import walter from "../assets/walter.jpg";
-import abdullah from "../assets/abdullah.jpg";
+import jere from "../assets/jere.jpg"
+import walter from "../assets/walter.jpg"
+import abdullah from "../assets/abdullah.jpg"
+import benjamin from "../assets/benjamin.jpeg"
+
 export const About = () => {
-  return (
-    <div>
-      <div id="myCarousel" className="carousel slide" data-ride="carousel">
-        <ol className="carousel-indicators">
-          <li
-            data-target="#myCarousel"
-            data-slide-to="0"
-            className="active"
-          ></li>
-          <li data-target="#myCarousel" data-slide-to="1"></li>
-          <li data-target="#myCarousel" data-slide-to="2"></li>
-          <li data-target="#myCarousel" data-slide-to="3"></li>
-        </ol>
-        <div className="carousel-inner">
-          <div className="carousel-item active">
-            <img className="kuva" alt="jere" src={jere} />
+  return <div>
+  <div id="myCarousel" className="carousel slide" data-ride="carousel">
+      <ol className="carousel-indicators">
+        <li data-target="#myCarousel" data-slide-to="0" className="active"></li>
+        <li data-target="#myCarousel" data-slide-to="1"></li>
+        <li data-target="#myCarousel" data-slide-to="2"></li>
+        <li data-target="#myCarousel" data-slide-to="3"></li>
+      </ol>
+      <div className="carousel-inner">
+        <div className="carousel-item active">
+          <img className="kuva" alt="jere" src={jere}/>
             <div className="container">
               <div className="carousel-caption">
                 <h1 className="text captionText">Jere Saarelma</h1>
               </div>
             </div>
-          </div>
-          <div className="carousel-item">
-            <img className="kuva" alt="walter" src={walter} />
+        </div>
+        <div className="carousel-item">
+          <img className="kuva" alt="walter" src={walter}/>
             <div className="container">
               <div className="carousel-caption">
-                <h1 className="text captionText">Walter Ruoppa</h1>
+                <h1 className="text captionText">Walter Ruoppa </h1>
+                <h2 className="quotes"> Before you judge a man, walk a mile in his shoes. After that who cares?... He’s a mile away and you’ve got his shoes! </h2>
               </div>
+
             </div>
-          </div>
-          <div className="carousel-item">
-            <img className="kuva" alt="abdullah" src={abdullah} />
-            <div className="container">
-              <div className="carousel-caption">
-                <h1 className="text captionText">Abdullah Hinnawi</h1>
-              </div>
-            </div>
-          </div>
-          <div className="carousel-item">
-            <img
-              className="kuva"
-              src="https://cdn.discordapp.com/attachments/669861606221545478/674941265296752641/SPOILER_B885DA30-C2B4-45E2-8EB1-11A88019D4E4_1_105_c.jpeg"
-              alt="bennn"
-            />
-            <div className="container">
-              <div className="carousel-caption">
-                <h1 className="text captionText">Benjamin Bowo</h1>
-              </div>
+        </div>
+        <div className="carousel-item">
+          <img className="kuva"
+               alt="abdullah" src={abdullah}/>
+          <div className="container" >
+            <div className="carousel-caption">
+              <h1 className="text captionText">Abdullah Hinnawi</h1>
             </div>
           </div>
         </div>
-        <a
-          className="carousel-control-prev"
-          href="#myCarousel"
-          role="button"
-          data-slide="prev"
-        >
-          <span
-            className="carousel-control-prev-icon"
-            aria-hidden="true"
-          ></span>
-          <span className="sr-only">Previous</span>
-        </a>
-        <a
-          className="carousel-control-next"
-          href="#myCarousel"
-          role="button"
-          data-slide="next"
-        >
-          <span
-            className="carousel-control-next-icon"
-            aria-hidden="true"
-          ></span>
-          <span className="sr-only">Next</span>
-        </a>
+        <div className="carousel-item">
+          <img className="kuva"
+               src={benjamin}
+               alt="bennn"/>
+            <div className="container">
+              <div className="carousel-caption">
+                <h1  className="text captionText">Benjamin Bowo</h1>
+              </div>
+            </div>
+        </div>
+
       </div>
+      <a className="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
+        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span className="sr-only">Previous</span>
+      </a>
+      <a className="carousel-control-next" href="#myCarousel" role="button" data-slide="next">
+        <span className="carousel-control-next-icon" aria-hidden="true"></span>
+        <span className="sr-only">Next</span>
+      </a>
     </div>
-  );
+  </div>;
 };
