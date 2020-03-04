47384f75c0dccad20fccb2f3c01e750ccfe3c0a2
Jepu32 <43566068+Jepu32@users.noreply.github.com>
2020-03-04 14:20:52 +0200

    seatServiceReact luotu. hakee istumapaikat jne. kaikkia metodeja ei ole testattu

---
+++ src/main/java/com/msgroup/moviesurfer/FrontApp/src/services/seatServiceReact.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+const seatsUrl = "http://localhost:8080/api/seats";
+const reserveSeatUrl = "http://localhost:8080/api/seats/reserve";
+
+/**
+ * get all movies from seats table
+ * @returns {Promise<T>}
+ */
+const getAll = () => {
+    const request = axios.get(seatsUrl);
+    return request.then(response => response.data);
+};
+
+//not tested
+const getById = ({movie}) => {
+    const request = axios.get(`${seatsUrl}/${movie.movieId}`, movie);
+    return request.then(response => response.data);
+};
+
+//not tested
+const reserveSeat = seatObject => {
+    const request = axios.put(`${reserveSeatUrl}/${seatObject.id}`, seatObject);
+    return request.then(response => response.data);
+};
+
+export default {getAll, getById, reserveSeat};
