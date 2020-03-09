d12fee089f964c57f6294860b69c91451abd2612
Benjamin Bowo <ben.bowo@gmail.com>
2020-03-09 10:25:37 +0200

    NotFound Page

--- src/main/java/com/msgroup/moviesurfer/FrontApp/src/model/NotFound.js
+++ src/main/java/com/msgroup/moviesurfer/FrontApp/src/model/NotFound.js
@@ -1,19 +1,14 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import jere from "../assets/jere.jpeg";
-
+import page from "../assets/pagenotfound.jpg";
 
 class NotFound extends React.Component {
-    render()
-    {
-        return (
-            <div>
-                <img className="profilePicture" alt="jere" src={jere} />
-                <p style={{color: "blue"}}>not found</p>
-            </div>
-        )
-
-    }
-
+  render() {
+    return (
+      <div>
+        <img className="assetsImage" alt="jere" src={page} />
+      </div>
+    );
+  }
 }
 export default NotFound;
