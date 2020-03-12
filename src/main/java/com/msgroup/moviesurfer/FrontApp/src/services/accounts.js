27c4b341b28b132bd12b65529c32120bb4cf6263
Jepu32 <43566068+Jepu32@users.noreply.github.com>
2020-03-12 09:42:07 +0200

    comments updated

--- src/main/java/com/msgroup/moviesurfer/FrontApp/src/services/accounts.js
+++ src/main/java/com/msgroup/moviesurfer/FrontApp/src/services/accounts.js
@@ -1,21 +1,40 @@
 import axios from "axios";
 const baseUrl = "http://localhost:8080/api/movies";
 
+/**
+ * Get all movies from database.
+ * @returns {Promise<T>}
+ */
 const getAll = () => {
   const request = axios.get(baseUrl);
   return request.then(response => response.data);
 };
 
+/**
+ * Add movie to database
+ * @param newObject
+ * @returns {Promise<T>}
+ */
 const create = newObject => {
   const request = axios.post(baseUrl, newObject);
   return request.then(response => response.data);
 };
 
+/**
+ * Update/replace movie.
+ * @param newObject
+ * @returns {Promise<T>}
+ */
 const replace = newObject => {
-  const request = axios.put(`${baseUrl}/${newObject.id}`, newObject);
+  const request = axios.post(`${baseUrl}/${newObject.id}`, newObject);
   return request.then(response => response.data);
 };
 
+/**
+ * delete movie
+ * @param id
+ * @returns {Promise<T>}
+ */
 const remove = id => {
   const request = axios.delete(`${baseUrl}/${id}`);
   return request.then(response => response.data);
