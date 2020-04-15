a2317a6d6d3fe906f8d06cfd478b7c1735c53f91
Jepu32 <43566068+Jepu32@users.noreply.github.com>
2020-04-15 10:36:22 +0300

    test updated

--- src/test/java/com/msgroup/moviesurfer/LoginRequestTest.java
+++ src/test/java/com/msgroup/moviesurfer/LoginRequestTest.java
@@ -1,17 +1,13 @@
 package com.msgroup.moviesurfer;
 import com.msgroup.moviesurfer.model.LoginRequest;
-import org.junit.BeforeClass;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.validation.ConstraintViolation;
 import javax.validation.Validation;
 import javax.validation.Validator;
-import javax.validation.ValidatorFactory;
 
 import java.util.Set;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
 public class LoginRequestTest {
@@ -19,30 +15,13 @@ public class LoginRequestTest {
 
     private final Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
 
-    /*
-    @BeforeClass
-    public static void setUp(){
-        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
-        validator = factory.getValidator();
-    }
-
-
-     */
-
     @Test
     public void testLoginReq() throws Exception{
         LoginRequest loginRequest = new LoginRequest();
         loginRequest.setEmail("sdfs ");
         loginRequest.setPassword(" sdfsd");
 
-        //validator.validate returns null vaikka pitäis tulla vähintään tyhjä Set
         Set<ConstraintViolation<LoginRequest>> violations = validator.validate(loginRequest);
-
-
-       // assertEquals(2, violations.size());
         assertFalse(violations.isEmpty());
-
     }
-
-
 }
